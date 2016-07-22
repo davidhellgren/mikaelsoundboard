d251562ab49f0a53fd0cc1df0763677caf76a54f
davidhellgren <david.hellgren@hotmail.com>
2016-07-22 11:21:57 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function play(){
+       var audio = document.getElementById("halkbana");
+       audio.play();
+}
+function play2(){
+       var audio2 = document.getElementById("magiskt");
+       audio2.play();
+}
